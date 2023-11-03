ad177d7211de0e47b056503dab8bd32d48d25a8c
wowalswjd <alswjd5491@naver.com>
2023-11-04 05:00:56 +0900

    :tada: typescript 프로젝트 기본 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      typescript 초기 셋팅
+    </div>
+  );
+}
+
+export default App;
