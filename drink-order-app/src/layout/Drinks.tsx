dd4e02fcef83d8660d7c8e16f6369200107a91de
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2023-11-03 13:33:36 +0900

    ✨ : tsx로 파일명 변경 & 타입 추가

---
+++ drink-order-app/src/layout/Drinks.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import Card from "../components/Card";
+import { BestList, CoffeeList, ShakeList, BubbleteaList } from "../DrinkList";
+
+interface DrinkProps {
+  type: "best" | "coffee" | "shake" | "bubbletea";
+}
+
+// 음료 타입에 따른 음료 리스트 반환
+const Drinks = ({ type }: DrinkProps) => {
+  let drinkList;
+
+  switch (type) {
+    case "best":
+      drinkList = BestList;
+      break;
+    case "coffee":
+      drinkList = CoffeeList;
+      break;
+    case "shake":
+      drinkList = ShakeList;
+      break;
+    case "bubbletea":
+      drinkList = BubbleteaList;
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <Container>
+      {drinkList?.map((drink, idx) => (
+        <Card id={idx} name={drink.name} price={drink.price} img={drink.img} />
+      ))}
+    </Container>
+  );
+};
+
+export default Drinks;
+
+const Container = styled.div``;
---
+++ drink-order-app/src/pages/Home.tsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import Layout from "../layout/Layout";
+import Header from "../components/Header";
+import Drinks from "../layout/Drinks";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      {/* 클릭 시 글자 색이 핑크색으로 변경 */}
+      <Tablist>
+        <NavLink
+          to="/best"
+          style={({ isActive }) =>
+            isActive
+              ? { color: "var(--pink)", textDecoration: "none" }
+              : { textDecoration: "none" }
+          }
+        >
+          베스트
+        </NavLink>
+        <NavLink
+          to="/coffee"
+          style={({ isActive }) =>
+            isActive
+              ? { color: "var(--pink)", textDecoration: "none" }
+              : { textDecoration: "none" }
+          }
+        >
+          커피
+        </NavLink>
+        <NavLink
+          to="/shake"
+          style={({ isActive }) =>
+            isActive
+              ? { color: "var(--pink)", textDecoration: "none" }
+              : { textDecoration: "none" }
+          }
+        >
+          셰이크
+        </NavLink>
+        <NavLink
+          to="/bubbletea"
+          style={({ isActive }) =>
+            isActive
+              ? { color: "var(--pink)", textDecoration: "none" }
+              : { textDecoration: "none" }
+          }
+        >
+          버블티
+        </NavLink>
+      </Tablist>
+      <Routes>
+        {/* 기본 라우트 설정 */}
+        <Route path="/" element={<Drinks type="best" />} />
+        <Route path="/best" element={<Drinks type="best" />}></Route>
+        <Route path="/coffee" element={<Drinks type="coffee" />}></Route>
+        <Route path="/shake" element={<Drinks type="shake" />}></Route>
+        <Route path="/bubbletea" element={<Drinks type="bubbletea" />}></Route>
+      </Routes>
+    </Layout>
+  );
+};
+
+export default Home;
+
+const Tablist = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-weight: 700;
+`;
